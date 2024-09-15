7580fb329785cbbc8c6132e73c37fc2fa1ca3724
algDevv <rahimmebrouka31@gmail.com>
2024-09-15 21:04:34 +0200

    header completed

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import "./index.css"
 import Home from './Pages/Home';
 import Gallery from './Pages/Gallery';
@@ -12,15 +12,19 @@ import Footer from './Components/Footer';
 function App() {
 
   return (
-    <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/gallery' element={<Gallery/>}/>
-        <Route path='/breed' element={<Breed/>} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <div className="relative bg-gray-900 min-h-screen overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full gradient-background">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/gallery' element={<Gallery />} />
+            <Route path='/breed' element={<Breed />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </div>
   )
 }
 
--- front-end/src/Components/Header.jsx
+++ front-end/src/Components/Header.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
-import reactLogo from '../assets/react.svg'
-import "../index.css"
+import React, { useState } from 'react'
+import logoDog from '../assets/logoDog.jpg'
+import { NavLink } from 'react-router-dom';
+import { MdClose, MdMenu } from "react-icons/md";
 const Header = () => {
-    return (
-        <section>
-            <div className="flex flex-row max-w-[1440px]">
-                <div className='flex flex-row items-center w-full px-10'>
-                    <img src={reactLogo} alt="React Logo" />
-                    <p className='text-red-600'>Home</p>
-                    <p>Kors</p>
-                </div>
-            </div>
+    const [menuOpen, setMenuOpen] = useState(false);
 
-        </section>
+    const handleMenu = () => {
+        setMenuOpen(prevState => !prevState);
+    }
+    const closeMenu = () => {
+        setMenuOpen(false);
+      };
+
+    return (
+        <header>
+            <div className="max_padd_container py-10 flex flex-row items-center justify-between">
+                <div className='flex flex-row items-center justify-between gap-3 p-1'>
+                    <div className='flex flex-row gap-3 items-center justify-around'>
+                        <img className='h-[50px] w-[50px] rounded-full' src={logoDog} alt="ReactLogo" />
+                        <h1 className='text-xl font-bold whitespace-nowrap'>Dog<span className='text-green-100 font-bold'>Industry.</span></h1>
+                    </div>
+                </div>
+                {/*mobile Menu*/}
+                <nav className='flex-col items-center justify-center gap-16 font-bold rounded-3xl p-1'>
+                    {menuOpen ? (<MdClose className='md:hidden cursor-pointer p-1 ring-1 ring-slate-900/30 h-10 w-10 rounded-full ' onClick={handleMenu} />
+                    ) : (<MdMenu className='md:hidden cursor-pointer p-1 ring-1 ring-slate-900/30 h-10 w-10 rounded-full ' onClick={handleMenu} />)}
+                </nav>
+                {menuOpen ? (
+                    <nav className="md:hidden flex items-center flex-col gap-y-12 fixed top-28 right-3 p-8 bg-white rounded-3xl shadow-md w-48 medium-16 ring-1 ring-slate-900/5 transition-all duration-300">
+                        <NavLink to={'/'} onClick={closeMenu} className={({ isActive }) => isActive ? "active-link" : ""} ><div className='font-bold text-lg'>Home</div></NavLink>
+                        <NavLink to={'/gallery'} onClick={closeMenu} className={({ isActive }) => isActive ? "active-link" : ""} ><div className='font-bold text-lg'>Gallery</div></NavLink>
+                        <NavLink to={'/breed'} onClick={closeMenu} className={({ isActive }) => isActive ? "active-link" : ""} ><div className='font-bold text-lg'>Breed</div></NavLink>
+                    </nav>
+                ) : ""}
+
+
+                {/*Laptop Menu*/}
+                <nav className='hidden md:flex flex-row items-center px-10 gap-16 font-bold rounded-3xl'>
+                    <NavLink to={'/'} className={({ isActive }) => isActive ? "active-link" : ""} ><div className='font-bold text-lg'>Home</div></NavLink>
+                    <NavLink to={'/gallery'} className={({ isActive }) => isActive ? "active-link" : ""} ><div className='font-bold text-lg'>Gallery</div></NavLink>
+                    <NavLink to={'/breed'} className={({ isActive }) => isActive ? "active-link" : ""} ><div className='font-bold text-lg'>Breed</div></NavLink>
+                </nav>
+            </div>
+        </header>
     );
 }
 
